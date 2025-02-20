a522752266709faca122346cb82c192abbffad8e
arturoxbar <101169728+arturoxbar@users.noreply.github.com>
2025-02-20 15:50:55 -0400

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,32 +1,40 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-export interface RecipeInterface extends mongoose.Document {
-    name: string;
-    description: string;
-    steps: string;
-    ingridients: string;
-    image?: string;
-    owner: mongoose.Types.ObjectId;
-    fav: boolean;
-    categories: [string];
+
+export interface UserInterface {
+    username: string;
+    password: string;
+    email: string;
+    verifyPassword: (password: string) => Promise<boolean>;
 }
 
-const RecipeSchema = new mongoose.Schema<RecipeInterface>(
-    {
+// Definición del esquema de usuario
+const UserSchema = new mongoose.Schema<UserInterface>({
+    username: { type: String, required: true, unique: false },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, unique: false },
+});
 
-        name: { type: String, required: true },
-        description: { type: String, required: true },
-        image: { type: String, required: false, default: "no image" },
-        steps: { type: String, required: true, default: null },
-        ingridients: { type: String, required: true, default: null },
-        owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-        categories: { type: [String] },
-        fav: { type: Boolean, default: false }
-    },
-    {
-        timestamps: true,
+// Middleware para hashear la contraseña antes de guardar el usuario
+UserSchema.pre("save", async function (next) {
+    // Solo hashear la contraseña si ha sido modificada
+    if (!this.isModified("password")) {
+        return next();
     }
-);
+    // Generar un salt y hashear la contraseña
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    // Continuar con el proceso de guardado
+    next();
+});
 
-const Recipe = mongoose.model<RecipeInterface>("Recipe", RecipeSchema);
-export default Recipe;
+// Método para verificar la contraseña
+UserSchema.methods.verifyPassword = async function (password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+};
+
+// Creación del modelo de usuario
+const User = mongoose.model<UserInterface>("User", UserSchema);
+
+export default User;
